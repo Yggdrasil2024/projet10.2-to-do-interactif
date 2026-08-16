eefa494eb5821ffd90cf68476e1bd7bc0c77a4ba
BIKOUTA Guyverna Mon-Mot Désir <bikoutaguyverna@gmail.com>
2026-08-16 21:23:41 +0100

    feat: l'ajout fonctionne

--- script.js
+++ script.js
@@ -4,13 +4,6 @@ const formular = document.getElementById("formular");
 const onGoingTasks = document.getElementById("ongoing__list");
 const completedTasks = document.getElementById("completed__list");
 const taskToSave = document.getElementById("task_title");
-//objet taches
-const newTask = {
-  id: "",
-  label: "",
-  isCompleted: false,
-};
-
 
 //ajout d'un tache depuis formulaire
 formular.addEventListener("submit", (event) => {
@@ -18,35 +11,38 @@ formular.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const taskTitle = taskToSave.value;
-  newTask.id = idGenerator();
-  newTask.label = taskTitle;
-  newTask.isCompleted = false;
-  //on ajoute le tous dans les taches
- tasks.unshift(newTask);
- //alert(`${newTask.id} ${newTask.label} ${newTask.isCompleted}`);
-//alert(JSON.stringify(tasks));
+  //objet taches
+  const newTask = {
+    id: idGenerator(),
+    label: taskTitle,
+    isCompleted: false,
+  };
 
+  //on ajoute le tous dans les taches
+  tasks.unshift(newTask);
+  //alert(`${newTask.id} ${newTask.label} ${newTask.isCompleted}`);
+  //alert(JSON.stringify(tasks));
 
   //on ajout à l'ui des taches
-  addToTasksUI(newTask, true);
+  addToTasksUI(newTask, false);
 
-  //saveTasks(tasks);
+  saveTasks(tasks);
+
+  formular.reset();
 });
+
 
 
 /**
  * fonction pour enregistrer les taches dans localStorage
- * 
+ *
  * @param {Array} array - les taches à enregistrer
- * @returns 
+ * @returns
  */
 const saveTasks = (array) => {
-  if (array.length !== 0) {
-    localStorage.setItem("saved_tasks") = JSON.stringify(tasks);
-    return true;
-  }
-  return false;
-}
+  localStorage.setItem("saved_tasks", JSON.stringify(tasks));
+  return true;
+};
 
 /**
  * fonction qui va renvoyer le html d'une tache
@@ -55,20 +51,22 @@ const saveTasks = (array) => {
 const buildHtmlFor = (task) => {
   return `<p>${task.label}</p>
             <div class="btn__area">
-              <button onclick="markAsCompleted('${task.id}')" class="btn" type="button">${task.isCompleted ? 'unmark': 'mark as completed'}</button>
+              <button onclick="markAsCompleted('${task.id}')" class="btn" type="button">${task.isCompleted ? "unmark" : "mark as completed"}</button>
               <button onclick="deleteTask('${task.id}')" class="btn btn_rm" type="button">delete</button>
             </div>`;
-}
+};
 
 //fonction qui ajoute une tache dans l'ui
-const addToTasksUI = (task, isNew) => {
+const addToTasksUI = (task, isCompleted = false) => {
+  let ParentElement = isCompleted ? completedTasks : onGoingTasks;
   const taskItem = document.createElement("li");
   taskItem.classList.add("card");
   taskItem.classList.add("list__item");
   taskItem.classList.add("appear");
-  taskItem.setAttribute('id', task.id);
+  taskItem.setAttribute("id", task.id);
   taskItem.innerHTML = buildHtmlFor(task);
-  return isNew ? onGoingTasks.appendChild(task) : completedTasks.appendChild(task);
+  if (task.length === 0) ParentElement.innerHTML = "";
+  return ParentElement.appendChild(taskItem);
 };
 
 /**
@@ -97,3 +95,20 @@ const idGenerator = () => {
   return id.join("");
 };
 
+const initUI = () => {
+  onGoingTasks.innerHTML = "";
+  completedTasks.innerHTML = "";
+
+  if (tasks.length > 0) {
+    completedTasks.classList.add("hidden");
+
+    tasks.forEach((element) => {
+      addToTasksUI(element, element.isCompleted);
+    });
+  } else {
+    onGoingTasks.innerHTML = `<p>Aucune tache pour le moment</p>`;
+    completedTasks.classList.add("hidden");
+  }
+};
+
+initUI();
